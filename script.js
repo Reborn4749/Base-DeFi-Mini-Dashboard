3fec076ee220462f01f109f0a8d595329bd82600
Reborn4797 <95199911+Reborn4749@users.noreply.github.com>
2026-05-01 11:55:27 +0700

    git commit -m "feat: add stake math logic"

--- script.js
+++ script.js
@@ -12,3 +12,15 @@ faucetBtn.addEventListener("click", function() {
     document.getElementById("balance").innerText = currentBalance; 
     alert("ได้รับ 100 $BASE!"); 
 });
+document.getElementById("stakeBtn").addEventListener("click", function() { 
+    let amt = Number(document.getElementById("amount").value); 
+    if(amt > 0 && amt <= currentBalance) { 
+        currentBalance -= amt; 
+        currentStaked += amt; 
+        document.getElementById("balance").innerText = currentBalance; 
+        document.getElementById("staked").innerText = currentStaked; 
+        document.getElementById("amount").value = ""; 
+    } else { 
+        alert("ยอดไม่ถูกต้อง หรือเงินไม่พอ!"); 
+    } 
+});
