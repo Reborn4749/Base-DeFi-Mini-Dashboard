60e293b073559a92685e2015768529e67876d8a4
Reborn4797 <95199911+Reborn4749@users.noreply.github.com>
2026-05-01 11:54:24 +0700

    git commit -m "feat: add connect wallet logic"

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
 let currentBalance = 0;
 let currentStaked = 0;
 const connectBtn = document.getElementById("connectBtn");
+connectBtn.addEventListener("click", function() { 
+    connectBtn.innerText = "Connected 🟢"; 
+    connectBtn.style.backgroundColor = "#28a745"; 
+    connectBtn.style.color = "white"; 
+});
